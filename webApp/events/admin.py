514d9efeafbeb8fa4dbd29a1c773fca2b924744a
SarwechAbro <sarwechabro@gmail.com>
2025-02-21 18:45:45 +0500

    first commit

---
+++ webApp/events/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Venue, Event
+from django.contrib.auth.models import Permission
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'codename')
+#admin.site.register(Permission, PermissionAdmin)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+	list_display = ('name', 'address', 'phone')
+	ordering = ('name',)
+	search_fields = ('name', 'address')
+#admin.site.register(Venue, VenueAdmin)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+	fields = (('name', 'venue'), 'event_date', 'description', 'manager', 'approved')
+	list_display = ('name', 'event_date', 'venue')
+	list_filter = ('event_date', 'venue')
+	ordering = ('event_date',)
---
+++ webApp/webApp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "My WebApp Administration Page"
+admin.site.site_title = ""
+admin.site.index_title = "Welcome To The Addmin Area..."
